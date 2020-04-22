45efd97ade46cd2f8b34f391eac16a242a97c38a
Miguel Perdigón <miguel.perdigon@hotmail.es>
2020-04-21 19:46:14 -0500

    added to generate a excel file

---
+++ utils/excel_manager.py
@@ -0,0 +1,30 @@
+#Desarrollado por el Licenciado Miguel Angel Perdigon Orta
+#Correo: miguelperdigon91@gmail.com
+#06/09/2018
+
+import pandas as pd
+from io import BytesIO
+from utils.constant import NORMAL_CELL_STYLE
+
+
+def generate(data_file_list, name_sheets, header_style=None):
+    file_buffer = BytesIO()
+    writer = pd.ExcelWriter(file_buffer, engine='xlsxwriter')
+    workbook = writer.book
+
+    if header_style:
+        header_format = workbook.add_format(header_style)
+
+    for i, data_file in enumerate(data_file_list):
+        df = pd.DataFrame(data_file)
+        df.to_excel(writer, name_sheets[i], index=False)
+
+        worksheet = writer.sheets[name_sheets[i]]
+        df.style.applymap(NORMAL_CELL_STYLE)
+
+        if header_style:
+            for col_num, value in enumerate(df.columns.values):
+                worksheet.write(0, col_num, value, header_format)
+
+    writer.save()
+    return file_buffer
